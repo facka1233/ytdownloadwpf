2be4607d25522f5416abaaa82d0d95d814ed9d33
facka1233 <fachrudinokta@gmail.com>
2022-11-19 21:58:12 +0700

    update

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -16,22 +15,3 @@ namespace kelompok_1
     {
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace kelompok_1
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
-}
->>>>>>> 77791fd (Perubahan icon di player)
