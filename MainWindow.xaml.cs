f7a5ba2badc6496370ef2e6b2075a84c8e564e9c
facka1233 <fachrudinokta@gmail.com>
2022-11-19 17:20:34 +0700

    update icon

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -41,3 +42,48 @@ namespace kelompok_1
         }
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kelompok_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVideo_Click(object sender, RoutedEventArgs e)
+        {
+            Progress progress = new Progress();
+            progress.Show();
+            this.Hide();
+        }
+
+        private void btnAudio_Click(object sender, RoutedEventArgs e)
+        {
+            Progress progress = new Progress();
+            progress.Show();
+            this.Hide();
+        }
+    }
+}
+>>>>>>> 77791fd (Perubahan icon di player)
--- packages/CSVideoPlayer.1.0.3/CSPlugins/FFmpeg/x64/Readme.txt
+++ packages/CSVideoPlayer.1.0.3/CSPlugins/FFmpeg/x64/Readme.txt
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 You can download the x64 version of the FFmpeg libraries from:
 https://www.microncode.com/downloads/ffmpeg58x64.7z
 
@@ -6,3 +7,13 @@ LGPL.
 
 You can add those libraries to your commercial projects by
 giving relevant credits.
+=======
+You can download the x64 version of the FFmpeg libraries from:
+https://www.microncode.com/downloads/ffmpeg58x64.7z
+
+The ffmpeg libraries licensed under
+LGPL.
+
+You can add those libraries to your commercial projects by
+giving relevant credits.
+>>>>>>> 77791fd (Perubahan icon di player)
